543b054170e2739e81219c8ceb04129b35f7503c
Oussama Teyib <oussama.teyib@gmail.com>
2026-04-30 23:00:46 +0000

    Create database class
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

---
+++ app/src/main/kotlin/com/oussamateyib/thoth/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,13 @@
+package com.oussamateyib.thoth.feature_note.data.data_source
+
+import androidx.room.RoomDatabase
+import androidx.room.vo.Database
+import com.oussamateyib.thoth.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract val noteDao: NoteDao
+}
