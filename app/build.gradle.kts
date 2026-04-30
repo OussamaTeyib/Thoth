0233ca5b702abde7fc0413b5b76432e5163e1b69
Oussama Teyib <oussama.teyib@gmail.com>
2026-04-30 20:49:35 +0000

    Create Note data class
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.room.common.jvm)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
