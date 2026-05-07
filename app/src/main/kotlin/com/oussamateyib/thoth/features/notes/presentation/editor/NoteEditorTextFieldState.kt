944e26c35e9c05ddcba23ea78608dccf9a90184f
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-07 23:12:12 +0000

    Remove unused import in NoteEditorTextFieldState
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
@@ -1,7 +1,5 @@
 package com.oussamateyib.thoth.features.notes.presentation.editor
 
-import com.oussamateyib.thoth.features.notes.domain.model.Note
-
 class NoteEditorTextFieldState(
     val text: String = "",
     val hint: String = "",
