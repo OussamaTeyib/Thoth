a7e71c91ca53329e5c38ab958e8718dfa5830be9
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-10 04:11:36 +0000

    Review `NoteEditorUiEvent`
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorUiEvent.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorUiEvent.kt
@@ -1,6 +1,5 @@
 package com.oussamateyib.thoth.features.notes.presentation.editor
 
 sealed class NoteEditorUiEvent {
-    data class ShowSnackbar(val message : String) : NoteEditorUiEvent()
     data object NoteSaved : NoteEditorUiEvent()
 }
