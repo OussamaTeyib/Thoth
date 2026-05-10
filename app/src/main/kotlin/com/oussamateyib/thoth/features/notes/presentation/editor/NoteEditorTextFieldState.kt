b895900071e9411fb4863c0dff0de50885c78838
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-10 03:01:30 +0000

    Fix typo
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
@@ -3,5 +3,5 @@ package com.oussamateyib.thoth.features.notes.presentation.editor
 data class NoteEditorTextFieldState(
     val text: String = "",
     val hint: String = "",
-    val isHintVisibile: Boolean = true
+    val isHintVisible: Boolean = true
 )
