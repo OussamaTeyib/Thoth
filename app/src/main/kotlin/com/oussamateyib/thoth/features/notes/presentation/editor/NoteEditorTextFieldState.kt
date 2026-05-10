dd20ce4ec4ce30eb32f4a72087c66b37e6895990
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-10 03:49:31 +0000

    Use string resources for text field hints
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/NoteEditorTextFieldState.kt
@@ -1,7 +1,9 @@
 package com.oussamateyib.thoth.features.notes.presentation.editor
 
+import androidx.annotation.StringRes
+
 data class NoteEditorTextFieldState(
     val text: String = "",
-    val hint: String = "",
+    @param:StringRes val hint: Int = 0,
     val isHintVisible: Boolean = true
 )
