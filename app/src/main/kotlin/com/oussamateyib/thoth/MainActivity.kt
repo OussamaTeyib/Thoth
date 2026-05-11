78d9d2182380303febec6418ce724a7195814388
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-11 03:52:24 +0000

    Register editor screen in the navigator
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/MainActivity.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.oussamateyib.thoth.features.notes.presentation.editor.NoteEditorScreen
 import com.oussamateyib.thoth.features.notes.presentation.list.NoteListScreen
+import com.oussamateyib.thoth.features.notes.presentation.util.NoteScreen
 import com.oussamateyib.thoth.ui.theme.ThothTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,8 +24,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThothTheme {
-                val navController = rememberNavController()
-                NoteListScreen(navController)
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = NoteScreen.ListScreen.route
+                    ) {
+                        composable(
+                            route = NoteScreen.ListScreen.route
+                        ) {
+                            NoteListScreen(navController = navController)
+                        }
+                        composable(
+                            route = NoteScreen.EditorScreen.route + "?noteId={noteId}",
+                            arguments = listOf(
+                                navArgument("noteId") {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                }
+                            )
+                        ) {
+                            NoteEditorScreen(navController = navController)
+                        }
+                    }
+                }
             }
         }
     }
