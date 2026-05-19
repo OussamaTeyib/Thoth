5c53fb6afc492cb854882449f1845679541a9374
Oussama Teyib <oussama.teyib@gmail.com>
2026-05-19 14:19:09 +0000

    Add white to default color palette
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/util/NoteColors.kt
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/presentation/editor/util/NoteColors.kt
@@ -5,7 +5,8 @@ import com.oussamateyib.thoth.ui.theme.LightGreen
 import com.oussamateyib.thoth.ui.theme.RedOrange
 import com.oussamateyib.thoth.ui.theme.RedPink
 import com.oussamateyib.thoth.ui.theme.Violet
+import com.oussamateyib.thoth.ui.theme.White
 
 object NoteColors {
-    val noteColors = listOf(RedPink, RedOrange, LightGreen, Violet, BabyBlue)
+    val noteColors = listOf(RedPink, RedOrange, LightGreen, Violet, BabyBlue, White)
 }
