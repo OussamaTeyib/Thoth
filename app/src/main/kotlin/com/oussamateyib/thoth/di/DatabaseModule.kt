b28d728abf069247553a2577575d0150692ef9f5
Oussama Teyib <oussama.teyib@gmail.com>
2026-06-01 00:58:14 +0000

    Refactor Room database setup and split Hilt DI modules

---
+++ app/src/main/kotlin/com/oussamateyib/thoth/di/DatabaseModule.kt
@@ -0,0 +1,30 @@
+package com.oussamateyib.thoth.di
+
+import android.app.Application
+import androidx.room.Room
+import com.oussamateyib.thoth.core.database.ThothDatabase
+import com.oussamateyib.thoth.features.notes.data.local.NoteDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application): ThothDatabase {
+        return Room.databaseBuilder(
+            app,
+            ThothDatabase::class.java,
+            ThothDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    fun provideNoteDao(db: ThothDatabase): NoteDao {
+        return db.noteDao
+    }
+}
---
+++ app/src/main/kotlin/com/oussamateyib/thoth/features/notes/di/NotesModule.kt
@@ -0,0 +1,20 @@
+package com.oussamateyib.thoth.features.notes.di;
+
+import com.oussamateyib.thoth.features.notes.data.local.NoteDao
+import com.oussamateyib.thoth.features.notes.data.repository.NoteRepositoryImpl
+import com.oussamateyib.thoth.features.notes.domain.repository.NoteRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NotesModule {
+    @Provides
+    @Singleton
+    fun provideNoteRepository(noteDao: NoteDao): NoteRepository {
+        return NoteRepositoryImpl(noteDao)
+    }
+}
