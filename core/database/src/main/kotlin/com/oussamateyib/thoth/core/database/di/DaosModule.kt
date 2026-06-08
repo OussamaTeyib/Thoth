d41fd7d5a6ae7dbe22ef1c8d34270fe6998fc7f2
Oussama Teyib <oussama.teyib@gmail.com>
2026-06-08 18:10:12 +0000

    Fix typo in filename
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- core/database/src/main/kotlin/com/oussamateyib/thoth/core/database/di/DaosModule.kt
+++ core/database/src/main/kotlin/com/oussamateyib/thoth/core/database/di/DaosModule.kt
@@ -6,11 +6,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object DoesModule {
+internal object DaosModule {
     @Provides
     fun provideNoteDao(
         database: ThothDatabase
