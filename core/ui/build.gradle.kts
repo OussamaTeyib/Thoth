ef77803bdb6b6db3209464855a0641be77806a8e
Oussama Teyib <oussama.teyib@gmail.com>
2026-06-18 02:55:51 +0000

    Extract note list items into `LazyListScope` extension
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core)
-    implementation(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
