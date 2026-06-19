76171e0660b5402d9afa11ca72a6ded88b0207b3
Oussama Teyib <oussama.teyib@gmail.com>
2026-06-19 15:29:00 +0000

    Review `NoteItem`
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -30,13 +30,12 @@ dependencies {
     api(projects.core.model)
 
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.core)
+
     api(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
     api(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.geometry)
     api(libs.androidx.compose.ui.graphics)
     api(libs.androidx.compose.ui.text)
     implementation(libs.androidx.compose.ui.unit)
--- core/ui/src/main/kotlin/com/oussamateyib/thoth/core/ui/NoteItem.kt
+++ core/ui/src/main/kotlin/com/oussamateyib/thoth/core/ui/NoteItem.kt
@@ -1,12 +1,14 @@
 package com.oussamateyib.thoth.core.ui
 
-import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,18 +16,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.ColorUtils
 import com.oussamateyib.thoth.core.model.data.Note
 
 @Composable
@@ -33,43 +29,24 @@ fun NoteItem(
     note: Note,
     modifier: Modifier = Modifier,
     cornerRadius: Dp = 10.dp,
-    cutCornerSize: Dp = 30.dp,
     onDeleteClick: () -> Unit
 ) {
     Box(
         modifier = modifier
+            .background(
+                color = Color(note.color),
+                shape = RoundedCornerShape(cornerRadius)
+            )
+            .border(
+                width = 1.dp,
+                color = Color.Black,
+                shape = RoundedCornerShape(cornerRadius)
+            )
     ) {
-        Canvas(
-            modifier = Modifier.matchParentSize() // Avoid influencing the Box's size
-        ) {
-            val clipPath = Path().apply {
-                lineTo(size.width - cutCornerSize.toPx(), 0f)
-                lineTo(size.width, cutCornerSize.toPx())
-                lineTo(size.width, size.height)
-                lineTo(0f, size.height)
-                close()
-            }
-
-            clipPath(clipPath) {
-                drawRoundRect(
-                    color = Color(note.color),
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-                drawRoundRect(
-                    color = Color(
-                        ColorUtils.blendARGB(note.color, android.graphics.Color.BLACK, 0.3f)
-                    ),
-                    topLeft = Offset(size.width - cutCornerSize.toPx(), -100f),
-                    size = Size(cutCornerSize.toPx() + 100f, cutCornerSize.toPx() + 100f),
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-            }
-        }
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
-                .padding(end = 32.dp)
         ) {
             Text(
                 text = note.title,
