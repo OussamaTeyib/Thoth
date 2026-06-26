dd84ff3e9fdc1fd19a9a9309af033966e3278f18
Oussama Teyib <oussama.teyib@gmail.com>
2026-06-26 23:59:14 +0000

    Set lint targetSdk to 37 for library modules
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -14,6 +14,7 @@ abstract class AndroidLibraryConventionPlugin : Plugin<Project> {
 
             extensions.configure<LibraryExtension> {
                 configureKotlinAndroid(this)
+                lint.targetSdk = 37
             }
         }
     }
