1ed64a25ab0fe1dd95d8630344b0bf41ee6d033b
Oussama Teyib <oussama.teyib@gmail.com>
2026-07-05 03:35:06 +0000

    Update launcher icon and background color
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- build-logic/convention/src/main/kotlin/AndroidLintConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLintConventionPlugin.kt
@@ -34,4 +34,5 @@ private fun Lint.configure() {
     disable += "AndroidGradlePluginVersion"
     disable += "GradleDependency"
     disable += "NewerVersionAvailable"
+    disable += "VectorPath"
 }
