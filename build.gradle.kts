62bcf7bd6a8c44169ab9a064db2200df67d04d37
Oussama Teyib <oussama.teyib@gmail.com>
2026-07-08 18:57:10 +0000

    Ensure build scripts use the latest `kotlin-metadata-jvm` version
    Signed-off-by: Oussama Teyib <oussama.teyib@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,9 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.room) apply false
 }
+
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.metadata.jvm)
+    }
+}
